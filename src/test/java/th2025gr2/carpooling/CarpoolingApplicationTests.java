4607bfbce808d1e4ad7ec9abaee478d55ea3e978
Dawid Florczyk <dawidflorczyk0@gmail.com>
2025-11-09 19:23:38 +0100

    Initial Commit

---
+++ src/test/java/th2025gr2/carpooling/CarpoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package th2025gr2.carpooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
