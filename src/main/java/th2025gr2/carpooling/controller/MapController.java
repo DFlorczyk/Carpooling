dcf48c1ca7d1f2b9437a8279cf231bf179ee22e5
Piotr Wawrzak <piotr.wawrzak521@gmail.com>
2025-11-18 22:27:16 +0100

    Basic map testing

---
+++ src/main/java/th2025gr2/carpooling/controller/MapController.java
@@ -0,0 +1,19 @@
+package th2025gr2.carpooling.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @Value("${google.maps.api.key}")
+    private String apiKey;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("mapsApiKey", apiKey);
+        return "index";
+    }
+}
---
+++ src/main/java/th2025gr2/carpooling/controller/Ride.java
@@ -0,0 +1,4 @@
+package th2025gr2.carpooling.controller;
+
+public class Ride {
+}
