ec571e81d9b33508695d05c8e23dbaf6bb1467dd
Dawid Florczyk <dawidflorczyk0@gmail.com>
2026-01-14 10:35:20 +0100

    Added simple profile editing, changed how ride history looks

--- src/main/java/th2025gr2/carpooling/controller/ProfileController.java
+++ src/main/java/th2025gr2/carpooling/controller/ProfileController.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import th2025gr2.carpooling.model.User;
 import th2025gr2.carpooling.repository.UserRepository;
 import th2025gr2.carpooling.security.UserDetailsWithId;
@@ -27,4 +29,35 @@ public class ProfileController {
 
         return "layout";
     }
+    @GetMapping("/profile/edit")
+    public String editProfile(@AuthenticationPrincipal UserDetailsWithId userDetails,
+                              Model model) {
+
+        User user = userRepository.findById(userDetails.getId())
+                .orElseThrow();
+
+        model.addAttribute("pageTitle", "Edit Profile");
+        model.addAttribute("view", "user-details");
+        model.addAttribute("user", user);
+
+        return "layout";
+    }
+    @PostMapping("/profile/edit")
+    public String saveProfileChanges(@AuthenticationPrincipal UserDetailsWithId userDetails,
+                                     @ModelAttribute User formUser) {
+
+        User user = userRepository.findById(userDetails.getId())
+                .orElseThrow();
+
+        user.setName(formUser.getName());
+        user.setSurname(formUser.getSurname());
+        user.setEmail(formUser.getEmail());
+        user.setPhoneNumber(formUser.getPhoneNumber());
+
+        userRepository.save(user);
+
+        return "redirect:/profile";
+    }
+
+
 }
