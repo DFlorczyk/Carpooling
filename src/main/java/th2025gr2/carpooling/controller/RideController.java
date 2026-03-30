22aa47e200c1e9804fde00903fc389a4eb04c486
Piotr Wawrzak <piotr.wawrzak521@gmail.com>
2026-03-30 20:05:29 +0200

    Added route list, route displaying

--- src/main/java/th2025gr2/carpooling/controller/RideController.java
+++ src/main/java/th2025gr2/carpooling/controller/RideController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import th2025gr2.carpooling.model.Ride;
-import th2025gr2.carpooling.service.RideService;
+import th2025gr2.carpooling.controller.RideController;
 import th2025gr2.carpooling.dto.RideDTO;
 
 import java.util.List;
