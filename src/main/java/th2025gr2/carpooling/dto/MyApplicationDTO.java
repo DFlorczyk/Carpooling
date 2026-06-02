b813a6ccebde7bdb7a6130e420879eb55a031cd5
Piotr Wawrzak <piotr.wawrzak521@gmail.com>
2026-06-02 20:33:47 +0200

    Drive requests

---
+++ src/main/java/th2025gr2/carpooling/dto/MyApplicationDTO.java
@@ -0,0 +1,33 @@
+package th2025gr2.carpooling.dto;
+
+public class MyApplicationDTO {
+    public Long id;
+    public Long rideId;
+    public String status;
+    public Double pickupLatitude;
+    public Double pickupLongitude;
+    public Double dropoffLatitude;
+    public Double dropoffLongitude;
+    public Double rideStartLatitude;
+    public Double rideStartLongitude;
+    public Double rideEndLatitude;
+    public Double rideEndLongitude;
+
+    public MyApplicationDTO(Long id, Long rideId, String status,
+                             Double pickupLatitude, Double pickupLongitude,
+                             Double dropoffLatitude, Double dropoffLongitude,
+                             Double rideStartLatitude, Double rideStartLongitude,
+                             Double rideEndLatitude, Double rideEndLongitude) {
+        this.id = id;
+        this.rideId = rideId;
+        this.status = status;
+        this.pickupLatitude = pickupLatitude;
+        this.pickupLongitude = pickupLongitude;
+        this.dropoffLatitude = dropoffLatitude;
+        this.dropoffLongitude = dropoffLongitude;
+        this.rideStartLatitude = rideStartLatitude;
+        this.rideStartLongitude = rideStartLongitude;
+        this.rideEndLatitude = rideEndLatitude;
+        this.rideEndLongitude = rideEndLongitude;
+    }
+}
